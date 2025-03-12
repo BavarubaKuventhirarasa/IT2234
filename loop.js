31bbabdd01820e82d786e600f71d914a6797fa71
BavarubaKuventhirarasa <bavarubakuventhirarasa@gmail.com>
2025-03-12 17:35:12 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,20 @@
+//print 1 to 10 numbers using loop
+let i;
+for(i=1;i<=10;i++)
+{
+	console.log(i);
+}
+//print 1 to 10 numbers in reverse
+let j;
+for(j=10;j>=1;j++)
+{
+	console.log(j);
+}
+//print the odd numbers
+let k=1;
+while(k<=20)
+{
+	if(k%2 != 0)
+		console.log(k);
+	k++;
+}
